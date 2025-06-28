05df9133fd656a8cdf0d2b1def5d12b10dc1836c
BuildTools <unconfigured@null.spigotmc.org>
2025-06-28 10:14:49 -0500

    better code verifier

--- pkce-authenticator.js
+++ pkce-authenticator.js
@@ -1,14 +1,21 @@
 export function generateCodeVerifier(length = 128) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+  let codeVerifier = '';
   const array = new Uint8Array(length);
   crypto.getRandomValues(array);
-  return btoa(String.fromCharCode(...array))
-    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+  for (let i = 0; i < length; i++) {
+    codeVerifier += possible.charAt(array[i] % possible.length);
+  }
+  return codeVerifier;
 }
 
 export async function generateCodeChallenge(codeVerifier) {
   const encoder = new TextEncoder();
   const data = encoder.encode(codeVerifier);
   const digest = await crypto.subtle.digest('SHA-256', data);
-  return btoa(String.fromCharCode(...new Uint8Array(digest)))
-    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+  const base64 = btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+  return base64;
 }
